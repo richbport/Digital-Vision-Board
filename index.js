3aff69d71277ac28750ebd7784001a349e7458ba
Rich <richbport@yahoo.com>
2026-02-16 22:11:08 -0500

    editItemById button with svg

--- index.js
+++ index.js
@@ -135,6 +135,16 @@ function displayItems() {
     <div class="item-description">${
       item.description /*Item title selected by the user */ || "No description"
     }</div>
+    <div class="action-buttons">
+    <!-- *****This creates two action buttons for each item - an edit button that calls editItemById and a delete button called deleteItem
+    <button class="action-btn btn-secondary" onclick="editItemById('${
+      item.id
+    }')">
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+      <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+    </svg>
+    </button>
   `;
   });
 }
