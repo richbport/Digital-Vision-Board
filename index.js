2c4323d30f684d9832e57a565174f53a7e2638df
Rich <richbport@yahoo.com>
2026-02-16 22:13:56 -0500

    deleteItem button svg

--- index.js
+++ index.js
@@ -145,6 +145,15 @@ function displayItems() {
       <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
     </svg>
     </button>
+    <button class="action-btn btn-secondary" onclick="deleteItem('${
+      item.id
+    }')">
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <polyline points="3 6 5 6 21 6"></polyline>
+      <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+      <line x1="10" y1="11" x2="10" y2="17"></line>
+      <line x1="14" y1="11" x2="14" y2="17"></line>
+    </svg>
   `;
   });
 }
