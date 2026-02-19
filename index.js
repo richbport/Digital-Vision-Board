915521897435197b361f7fd8228d24691b4ef417
Rich <richbport@yahoo.com>
2026-02-19 18:39:26 -0500

    function calls added

--- index.js
+++ index.js
@@ -214,11 +214,14 @@ function saveItem() {
     const itemIndex = visionBoardItems.findIndex((i) => i.id === editingItemId);
     if (itemIndex !== -1) {
       // If is found then replace the current item to the new one (item)
-      visionBoardItems(itemIndex) = item;
+      visionBoardItems[itemIndex] = item;
     }
   } else {
     // If is not found then it means is a new one and in that case we want to push the created item to the visionBoardItems (the array that ??)
     visionBoardItems.push(item);
   }
-
 }
+
+saveItemsToStorage();
+displayItems();
+closeModal();
